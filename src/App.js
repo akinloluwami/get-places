b50afe06ee633d28119cdc77c1397202dbcba471
Akinkunmi <akinkunmiisrael41@gmail.com>
2020-12-11 02:41:26 +0000

    idek

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const api = {
-    baseUrl: "https://restcountries.eu/rest/v2/"
-}
+  baseUrl: "https://restcountries.eu/rest/v2/name/",
+};
 
 function App() {
-    const [query, setQuery] = useState('');
-    const [country, setCountry] = useState({});
+  const [query, setQuery] = useState("");
+  const [country, setCountry] = useState({});
 
-    return (
-        <div>
-            
-        </div>
-    )
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.baseUrl}${query}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setCountry(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="search_box">
+        <input
+          type="text"
+          className="search_bar"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
+          </div>
+          <p>{country[0].topLevelDomain}</p>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
